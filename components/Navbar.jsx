fdf21303bf4437ba2218b5dc46feb61ab34c71e0
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-06 16:51:11 -0500

    Continue to expand sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -36,16 +36,22 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'></div>
-
-      <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
-        <div className=''>
-          <div className='flex w-full items-center justify-between'>
-            <Image src='/assets/navLogo.png' alt='/' width='87' height='35' />
-            <div className=''>
-              <AiOutlineClose size={25} />
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+          <div className=''>
+            <div className='flex w-full items-center justify-between'>
+              <Image src='/assets/navLogo.png' alt='/' width='87' height='35' />
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose size={25} />
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>
+                Let's build something great together
+              </p>
             </div>
           </div>
+          <div className=''></div>
         </div>
       </div>
     </div>
