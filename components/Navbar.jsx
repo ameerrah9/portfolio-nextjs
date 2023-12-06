294823e40a6486a75a57336896229ea31614ee80
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-06 17:26:51 -0500

    Add conditional styling for Mobile Nav background

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,8 +7,16 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Navbar = () => {
+  const [nav, setNav] = React.useState(false);
+
+  const handleNav = () => {
+    setNav(true);
+  };
+
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      {' '}
+      {/* Mobile Nav */}
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Image src='/assets/navLogo.png' alt='/' width='125' height='50' />
         <div>
@@ -33,18 +41,23 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div className='md:hidden'>
+          <div onClick={handleNav} className='md:hidden'>
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
-
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+      {/* Mobile Nav */}
+      <div
+        className={!nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}
+      >
         <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
           <div className=''>
             <div className='flex w-full items-center justify-between'>
               <Image src='/assets/navLogo.png' alt='/' width='87' height='35' />
-              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <div
+                onClick={handleNav}
+                className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
+              >
                 <AiOutlineClose size={25} />
               </div>
             </div>
