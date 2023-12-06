634f4d2a0f83b8a25bcb13470a36c2633096a4d8
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-06 17:30:53 -0500

    Add conditional styling for Mobile Nav sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -48,9 +48,15 @@ const Navbar = () => {
       </div>
       {/* Mobile Nav */}
       <div
-        className={!nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}
+        className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}
       >
-        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+        <div
+          className={
+            nav
+              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+          }
+        >
           <div className=''>
             <div className='flex w-full items-center justify-between'>
               <Image src='/assets/navLogo.png' alt='/' width='87' height='35' />
