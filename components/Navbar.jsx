cfe215ac1ab654831cdada91863c9760b05df893
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-06 15:14:36 -0500

    Create web menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,29 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100] border-red-800'>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image src='/assets/navLogo.png' alt='/' width='100' height='10' />
+        <Image src='/assets/navLogo.png' alt='/' width='125' height='50' />
         <div>
-          <ul>
+          <ul className='hidden md:flex space-x-4'>
             <Link href='/'>
               <li className='m1-10 text-sm uppercase hover:border-b'>Home</li>
+            </Link>
+            <Link href='/'>
+              <li className='m1-10 text-sm uppercase hover:border-b'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='m1-10 text-sm uppercase hover:border-b'>Skills</li>
+            </Link>
+            <Link href='/'>
+              <li className='m1-10 text-sm uppercase hover:border-b'>
+                Projects
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='m1-10 text-sm uppercase hover:border-b'>
+                Contact
+              </li>
             </Link>
           </ul>
         </div>
