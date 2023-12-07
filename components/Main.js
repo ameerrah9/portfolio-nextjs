504b55b0c3d4e920669c729764fee900bde6c430
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-06 20:26:24 -0500

    Add links to main component

--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Main = () => {
   return (
@@ -8,6 +11,29 @@ const Main = () => {
           <p className='uppercase text-sm tracking-widest text-gray-600 '>
             LET'S BUILD SOMETHING GREAT TOGETHER
           </p>
+          <h1 className='py-4 text-gray-700'>
+            Hi, I'm <span className='text-[#00DF9A]'>Ghameerah</span>
+          </h1>
+          <h1 className='py-2 text-gray-700'>A Software Developer</h1>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto '>
+            I like building innovative solutions to technologically challenging
+            problems with motivated, intuitive people who share my passion.
+          </p>
+          {/* Icons */}
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaLinkedinIn size={25} />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaGithub size={25} />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineMail size={25} />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <BsFillPersonLinesFill size={25} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
